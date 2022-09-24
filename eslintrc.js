fbbe175fc53e06a2d5cbea31402c328ac562345f
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2022-09-24 18:53:19 +0200

    eslint config file format changed to js 🔧

---
+++ eslintrc.js
@@ -0,0 +1,68 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true,
+    "node": true
+  },
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "semi": [
+      "error",
+      "never"
+    ],
+    "arrow-parens": [
+      "error"
+    ],
+    "arrow-spacing": [
+      "error"
+    ],
+    "camelcase": [
+      "error"
+    ],
+    "default-case": [
+      "error"
+    ],
+    "no-empty": [
+      "error"
+    ],
+    "no-redeclare": [
+      "error"
+    ],
+    "no-unused-vars": [
+      "error"
+    ],
+    "no-var": [
+      "error"
+    ],
+    "object-curly-spacing": [
+      "error",
+      "always"
+    ],
+    "prefer-destructuring": [
+      "error"
+    ],
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "space-before-function-paren": [
+      "error"
+    ],
+    "space-in-parens": [
+      "error"
+    ],
+    "spaced-comment": [
+      "error"
+    ],
+    "line-comment-position": [
+      "error"
+    ]
+  }
+}
